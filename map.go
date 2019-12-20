661ac3fc385dfa6311f8732f6dc8826f28113340
Marc Santiago <marcanthonysanti@gmail.com>
2019-12-20 15:10:02 -0500

    Collections: Remove unused interface

--- map.go
+++ map.go
@@ -10,11 +10,3 @@ type CounterMap interface {
 	Subtract(value Data)
 	Update(value Data)
 }
-
-type OrderedMap interface {
-	Iterable
-	Delete(key Data)
-	Get(key Data) (Data, bool)
-	Set(key Data, value Data)
-	String() string
-}
