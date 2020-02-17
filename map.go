11f4c38a0bdd2ab51443c2967f498e41cfa6ee75
Marc Santiago <marcanthonysanti@gmail.com>
2020-02-17 18:26:00 -0500

    Collections: Create a generic map interface and male CounterMap a superset

--- map.go
+++ map.go
@@ -1,13 +1,25 @@
 package collections
 
 // CounterMap mimics the Python Counter definitions
+// this also implements the collections.Map interface to be able to convert into a ChainMap
 type CounterMap interface {
 	Iterable
 	Delete(value Data)
 	Get(key Data) (Data, bool)
 	Items() []Element
 	MostCommon(n int) []Element
+	Set(key Data, value Data)
 	String() string
 	Subtract(value Data)
 	Update(value Data)
+}
+
+// Map is a genetic map
+type Map interface {
+	Iterable
+	Delete(value Data)
+	Get(key Data) (Data, bool)
+	Set(key Data, value Data)
+	String() string
+	Items() []Element
 }
