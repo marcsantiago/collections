1dffaa8e9cd02453e80cdfa0737290a6ab1a9044
Marc Santiago <marcanthonysanti@gmail.com>
2020-02-17 19:26:33 -0500

    ChainMap: Basic implementation

---
+++ chainmap/chainmap.go
@@ -0,0 +1,98 @@
+package chain
+
+import (
+	"encoding/json"
+
+	"github.com/marcsantiago/collections"
+)
+
+type Maps []collections.Map
+
+// New returns a new chain map
+// if no maps are passed i new chain map with an initialized map is return
+// else all the maps are merged into indexes
+// Lookups search the underlying mappings successively until a key is found. In contrast, writes, updates, and deletions only operate on the first mapping.
+func New(maps ...collections.Map) Maps {
+	var cMap []collections.Map
+	if len(maps) == 0 {
+		return append(cMap, collections.NewGenericMap())
+	}
+
+	for _, m := range maps {
+		cMap = append(cMap, m)
+	}
+	return cMap
+}
+
+// Delete removes the element from the first internal map
+func (m Maps) Delete(key collections.Data) {
+	m[0].Delete(key)
+}
+
+// Get retrieves a data value from the first internal map that contains they targeted key
+func (m Maps) Get(key collections.Data) (collections.Data, bool) {
+	for _, mm := range m {
+		value, ok := mm.Get(key)
+		if ok {
+			return value, ok
+		}
+	}
+	return nil, false
+}
+
+// NewChild returns a new ChainMap containing a new map followed by all of the maps in the current instance. If m is specified, it becomes the new map at the front of the list of mappings; if not specified, an empty dict is used
+func (m Maps) NewChild(mm collections.Map) Maps {
+	if mm == nil {
+		return append([]collections.Map{collections.NewGenericMap()}, m...)
+	}
+	return append([]collections.Map{mm}, m...)
+}
+
+// Parents returning a new ChainMap containing all of the maps in the current instance except the first one. This is useful for skipping the first map in the search.
+func (m Maps) Parents() Maps {
+	return m[1:]
+}
+
+// Set adds a key value pairing to the first internal map
+func (m Maps) Set(key collections.Data, value collections.Data) {
+	m[0].Set(key, value)
+}
+
+// String returns the JSON string representation of the map data
+func (m Maps) String() string {
+	b, _ := json.Marshal(m)
+	return string(b)
+}
+
+// Items returns the internal map as a set of elements
+func (m Maps) Items() []collections.Element {
+	var total int
+	for _, mm := range m {
+		total += mm.Len()
+	}
+
+	items := make([]collections.Element, 0, total)
+	for _, mm := range m {
+		for elem := range mm.Iterate() {
+			items = append(items, collections.Element{
+				Key:   elem.Key,
+				Value: elem.Value,
+			})
+		}
+	}
+	return items
+}
+
+// Iterate creates a channel to create an iterator for he Go range statement
+func (m Maps) Iterate() <-chan collections.Element {
+	ch := make(chan collections.Element)
+	go func() {
+		for _, mm := range m {
+			for elem := range mm.Iterate() {
+				ch <- collections.Element{Key: elem.Key, Value: elem.Value}
+			}
+		}
+		close(ch)
+	}()
+	return ch
+}
---
+++ chainmap/doc.go
@@ -0,0 +1,2 @@
+// ChainMap class is provided for quickly linking a number of mappings so they can be treated as a single unit. It is often much faster than creating a new dictionary and running multiple update() calls. A go parody of the Python implementation.
+package chain
